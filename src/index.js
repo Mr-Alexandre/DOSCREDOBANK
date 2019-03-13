7868d6e9c11b4805c68c310d1af39eab739745dd
Alexandr <mr.l.v.alexandre@gmail.com>
2019-03-13 18:38:31 +0600

    Design layout

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import html from './index.pug'
-import style from './index.scss'
-import $ from 'jquery'
-import './jquery-ui(custom).min.js'
+import html from './index.pug';
+import style from './index.scss';
+import $ from 'jquery';
+import './jquery-ui(custom).min.js';
+import './jquery.mask';
 
 
 $(document).ready(function() {
+    /* Animation */
+    $(".gc-stamp").addClass("gc-stamp_active");
+    setTimeout(function(){
+        $(".gc-title-cover__car").addClass("gc-title-cover__car_animate");
+    }, 200);
+    
+
+
     var labels = $("#calculation-contribution-range-box").find(".gc-slider-range__lables")[0];
     var prevlabel;
     var colorRed = '#e31e24';
+    var period = 1;
+
     $("#calculation-contribution-range").slider({
         classes: {
             "ui-slider": "gc-slider-range",
@@ -24,11 +35,53 @@ $(document).ready(function() {
         create: function( e, ui ) {
             prevlabel = labels.querySelectorAll(`[data-index="3"]`);
             $(prevlabel).css({"color": colorRed});
+
+            period = Number( $(prevlabel).text() );
         },
         slide: function (e, ui) {
             $(prevlabel).css({"color": ''});
             prevlabel = labels.querySelectorAll(`[data-index="${ui.value}"]`);
             $(prevlabel).css({"color": colorRed});
+
+            period = Number( $(prevlabel).text() );
+            recalculation();
         }
     })
+
+    /* Inputs mask */
+    $(".phone-mask").mask("+996 (999) 999-999");
+    $('.money-mask').mask('000 000 000 000 000', {reverse: true});
+
+    $("#opening-amount").keyup(function(){
+        recalculation();
+    });
+    $("#valute-usd").change(function(){
+        recalculation();
+    });
+    $("#valute-kgs").change(function(){
+        recalculation();
+    });
+
+    
+    function recalculation(){
+        /* Calc contribution */
+        var kgs = $("#valute-kgs").is(":checked");
+        var usd = $("#valute-usd").is(":checked");
+        var rate;
+        var netProfitHTML = $("#net-profit");
+        var totalAmountHTML = $("#total-amount");
+        
+        if (kgs){
+            rate = 13;
+        }else{
+            rate = 2;
+        }
+        var openingAmount = Number($("#opening-amount").val().replace(/\s/g, ''));
+
+        // Вознаграждение = (Сумма на депозите x 10% x 30 дней) / 360 дней
+        var totalAmount = ( openingAmount * rate * 30 )/ period;
+        $(totalAmountHTML).text(totalAmount.toFixed(2));
+        $(netProfitHTML).text( (totalAmount - openingAmount).toFixed(2) );
+    }
+
 });
